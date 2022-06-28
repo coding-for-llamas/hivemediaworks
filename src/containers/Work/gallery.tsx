af65e3db7c179b4c247bc58e98b7afa459e6aa8f
Rhayven <codingforllamas@gmail.com>
2022-06-28 18:17:48 -0400

    Added in more gallery photos

--- src/containers/Work/gallery.tsx
+++ src/containers/Work/gallery.tsx
@@ -1,21 +1,27 @@
 const gallery = [
   {
-    name: 'Test',
-    url: 'https://dl.dropboxusercontent.com/s/p8q5diyni6deo6h/HMW_Logo.png?dl=0',
-    alt: 'Test',
-    description: 'Test',
+    name: 'Brazilian Oil Painting',
+    url: 'https://dl.dropboxusercontent.com/s/l88oozacs39lx6w/Brazilian%20Oil%20Painting%20.Folk%20Art%20Painting%20Ink%20on%20Canvas%20Dimensions_%2040_22%20X%2030_22.jpg?dl=0',
+    alt: 'Painting depicting a brazilian town block with people in the streets',
+    description: 'Folk Art Painting - Ink on Canvas',
   },
   {
-    name: 'Test 2',
-    url: 'https://dl.dropboxusercontent.com/s/p8q5diyni6deo6h/HMW_Logo.png?dl=0',
-    alt: 'Test',
-    description: 'Test',
+    name: 'City Log Ink Painting',
+    url: 'https://dl.dropboxusercontent.com/s/h3jn9uvcjgso0hv/City%20log%20Folk%20Art%20Painting%20Ink%20on%20Canvas%20Dimensions_%2040_22%20X%2030_22.jpg?dl=0',
+    alt: 'Painting depicting a cityscape with skyscrapers',
+    description: 'Folk Art Painting - Ink on Canvas',
   },
   {
-    name: 'Test 3',
-    url: 'https://dl.dropboxusercontent.com/s/p8q5diyni6deo6h/HMW_Logo.png?dl=0',
-    alt: 'Test',
-    description: 'Test',
+    name: 'Deport',
+    url: 'https://dl.dropboxusercontent.com/s/erodvq79otvgo9d/Deport.Folk%20Art%20Painting%20Ink%20on%20Canvas%20Dimensions_%2040_22%20X%2030_22.jpg?dl=0',
+    alt: 'Painting depicting a port with a several ships and shipping containers stacked up',
+    description: 'Folk Art Painting - Ink on Canvas',
+  },
+  {
+    name: 'Figure in Red Cocktail Dress',
+    url: 'https://dl.dropboxusercontent.com/s/zswuo5wsdif3nvm/Figure%20in%20Red%20Cocktail%20Dress%20Oil%20on%20Canvas%20Signed%20Kaladek%20Measures_%2031_22%20ht%20X%2022_22Wdth.jpg?dl=0',
+    alt: 'Painting depicting a woman in a red coctail dress holding a black shawl',
+    description: 'Signed Kadalek Measures - Oil on Canvas',
   },
 ];
 
