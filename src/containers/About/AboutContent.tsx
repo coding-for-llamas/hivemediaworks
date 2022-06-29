ba9434d4ad3329761623af3c67b8a3ad4f90887d
Rhayven <codingforllamas@gmail.com>
2022-06-29 05:01:41 -0400

    Updates about page to inline more towards final

--- src/containers/About/AboutContent.tsx
+++ src/containers/About/AboutContent.tsx
@@ -6,21 +6,23 @@ const AboutContent = (): JSX.Element => (
         {' '}
         <span className="text-secondary">Us</span>
       </h1>
-      <h2 className="sm-heading">Lorem ipsum dolor sit amet.</h2>
+      <h2 className="sm-heading">Your time is precious, we can save you some.</h2>
     </div>
     <div className="about-info">
       <div className="about-intro">
-        {/* <img className="about-picture" 
-        src="https://dl.dropboxusercontent.com/s/p8q5diyni6deo6h/HMW_Logo.png?dl=0" alt="Hive Media Works Logo" /> */}
         <div className="about-text">
-          <h2>Lorem, ipsum dolor.</h2>
-          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
-            Deserunt ex, sed mollitia voluptas aperiam atque nemo nobis deleniti quasi! Porro cum unde sint sunt similique.</p>
+          <h2>Here at the Hive, we know <span className="text-secondary">deadlines</span> matter.</h2>
+          <br />
+          <p>We'll go to work to keep you from feeling the sting of corporate crunch.
+            We strive to provide time saving custom solutions and services to independent artists and creators. 
+            Come join our hive 
+            <span className="text-secondary"> <b>today</b></span>
+            </p>
         </div>
       </div>
       <div className="about-proficiency">
         <div className="about-proficiency--box-1">
-          <h3 className="underline">Services</h3>
+          <h3 className="underline">Customer Support</h3>
             <p>Lorem ipsum dolor sit amet.</p>
             <p>Autem officiis ratione dolorum minima.</p>
             <p>Dolor corporis commodi optio aliquam.</p>
@@ -28,14 +30,14 @@ const AboutContent = (): JSX.Element => (
             <p>Porro esse ullam laudantium incidunt.</p>
         </div>
         <div className="about-proficiency--box-2">
-          <h3 className="underline">Services 2</h3>
+          <h3 className="underline">Sales Coordination</h3>
             <p>Lorem ipsum dolor sit amet.</p>
             <p>Iste mollitia id repellat non!</p>
             <p>Dolorum dolor cumque quisquam reprehenderit.</p>
             <p>Quidem ipsam iure explicabo laborum.</p>
         </div>
         <div className="about-proficiency--box-3">
-          <h3 className="underline">Services 3</h3>
+          <h3 className="underline">Record Keeping</h3>
           <p>Lorem ipsum dolor sit amet.</p>
           <p>Eligendi facere ex odio deserunt.</p>
           <p>Aperiam numquam aliquid autem quasi.</p>
